298e61fa9a73c47a48cc45aedcf27bcdcad37211
texasjkee <gt.gluck@gmail.com>
2023-01-20 20:13:05 +0200

    Fix then method

--- db.js
+++ db.js
@@ -19,3 +19,4 @@ module.exports = {
     },
     getDb: () => dbConnection,
 }
+
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {connectToDb, getDb} = require('./db')
+const {connectToDb, getDb} = require('./db');  
 
 const PORT = 3000;
 
@@ -9,13 +9,24 @@ let db;
 
 connectToDb((err) => {
     if(!err) {
-app.listen(PORT, (err) => {
-    err ? console.log(err) : console.log(`Listening port ${PORT}`);
-});
+        app.listen(PORT, (err) =>{
+        err ? console.log(err) : console.log(`Listening port ${PORT}`);
+    });
     db = getDb();
     } else {
         console.log(`DB connection error: ${err}`)
     }
-})
+});
 
+app.get('/testPDR', (req, res) => {
+    const foo = [];
 
+    db
+        .collection('testsPDR')
+        .find() 
+        .forEach((testPDR) => foo.push(testPDR))
+        .then(() => {
+            res.status(200)
+            .json(foo)
+        })
+});
