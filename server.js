39738d17dd3c89d97d477250b075869a0127ae5f
texasjkee <gt.gluck@gmail.com>
2023-01-23 21:21:27 +0200

    Add models.js file and integrations mongoose

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const { ObjectID, ObjectId } = require('bson');
 const express = require('express');
-const {connectToDb, getDb} = require('./db');  
-
+const mongoose = require('mongoose')
+const { ObjectId } = require('mongodb');
+const testPDR = require('./models/testPDR');
+const { findByIdAndDelete } = require('./models/testPDR');
+       
 const PORT = 3000;
+const URL = 'mongodb://0.0.0.0:27017/testPDRbox';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+mongoose
+    .connect(URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(`DB connection error: ${err}`))
 
-let db;
-
-connectToDb((err) => {
-    if(!err) {
-        app.listen(PORT, (err) => {
-        err ? console.log(err) : console.log(`Listening port ${PORT}`);
-    });
-    db = getDb();
-    } else {
-        console.log(`DB connection error: ${err}`)
-    }
+app.listen(PORT, (err) => {
+    err ? console.log(err) : console.log(`Listening port ${PORT}`);
 });
 
 const handleError = (res, error) => {
@@ -25,61 +23,49 @@ const handleError = (res, error) => {
 }
 
 //route
-app.get('/testPDR', (req, res) => {
-    const foo = [];
-
-    db
-        .collection('testPDR')
+app.get('/testPDRs', (req, res) => {
+    testPDR
         .find() 
-        .forEach((testPDR) => foo.push(testPDR))
-        .then(() => {
-            res.status(200).json(foo)
+        .then((testPDR) => {
+            res.status(200).json(testPDR)
         })
         .catch (()=> ( handleError(res, 'Could not fetch the documents')))
 });
 
-app.get('/testPDR/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-    db
-        .collection('testPDR')
-        .findOne({_id: ObjectId(req.params.id)}) 
+app.get('/testPDRs/:id', (req, res) => {
+    testPDR
+        .findById(req.params.id) 
         .then((doc) => {
             res.status(200).json(doc)
         })
         .catch(() => (handleError(res, 'Could not fetch the documents')))
-    } else handleError(res, 'Wrong id')
 });
 
-app.delete('/testPDR/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-    db
-        .collection('testPDR')
-        .deleteOne({_id: ObjectId(req.params.id)}) 
-        .then((doc) => {
-            res.status(200).json(doc)
-        })
+app.delete('/testPDRs/:id', (req, res) => {
+        testPDR 
+            .findByIdAndDelete(req.params.id) 
+            .then((doc) => {
+                res.status(200).json(doc)
+            })
         .catch(() => (handleError(res, 'Could not fetch the documents')))
-    } else handleError(res, 'Wrong id')
 });
 
-app.post('/testPDR', (req, res) => {
-    db
-        .collection('testPDR')
-        .insertOne(req.body) 
-        .then((doc) => {
-            res.status(201).json(doc)
-        })
-        .catch(() => (handleError(res, 'Could not fetch the documents')))
+app.post('/testPDRs', (req, res) => {
+        const _testPDR = new testPDR(req.body)
+        
+        _testPDR
+            .save()
+            .then((doc) => {
+                res.status(201).json(doc)
+            })
+            .catch((err) => (handleError(res, err)))
 })
 
-app.patch('/testPDR/:id', (req, res) => {
-    if(ObjectId.isValid(req.params.id)) {
-    db
-        .collection('testPDR')
-        .updateOne({_id: ObjectId(req.params.id)}, {$set: req.body}) 
+app.patch('/testPDRs/:id', (req, res) => {
+        testPDR 
+        .findByIdAndUpdate(req.params.id, req.body) 
         .then((doc) => {
             res.status(200).json(doc)
         })
         .catch(() => (handleError(res, 'Could not fetch the documents')))
-    } else handleError(res, 'Wrong id')
 });
