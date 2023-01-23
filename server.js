a6a64041c6e3d3e0e99bbeb505d177c30cee5a4a
texasjkee <gt.gluck@gmail.com>
2023-01-23 16:52:33 +0200

    Add post and patch method

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const {connectToDb, getDb} = require('./db');
 const PORT = 3000;
 
 const app = express();
+app.use(express.json())
 
 let db;
 
@@ -53,10 +54,31 @@ app.delete('/testPDR/:id', (req, res) => {
     if(ObjectId.isValid(req.params.id)) {
     db
         .collection('testPDR')
-        .findOne({_id: ObjectId(req.params.id)}) 
-        .forEach((testPDR) => foo.push(testPDR))
-        .then(() => {
-            res.status(200).json(foo)
+        .deleteOne({_id: ObjectId(req.params.id)}) 
+        .then((doc) => {
+            res.status(200).json(doc)
+        })
+        .catch(() => (handleError(res, 'Could not fetch the documents')))
+    } else handleError(res, 'Wrong id')
+});
+
+app.post('/testPDR', (req, res) => {
+    db
+        .collection('testPDR')
+        .insertOne(req.body) 
+        .then((doc) => {
+            res.status(201).json(doc)
+        })
+        .catch(() => (handleError(res, 'Could not fetch the documents')))
+})
+
+app.patch('/testPDR/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)) {
+    db
+        .collection('testPDR')
+        .updateOne({_id: ObjectId(req.params.id)}, {$set: req.body}) 
+        .then((doc) => {
+            res.status(200).json(doc)
         })
         .catch(() => (handleError(res, 'Could not fetch the documents')))
     } else handleError(res, 'Wrong id')
