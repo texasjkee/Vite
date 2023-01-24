ba9dec8dd63931632283f7360697c69a64f9f993
texasjkee <gt.gluck@gmail.com>
2023-01-24 13:43:20 +0200

    Add new URL

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const testPDR = require('./models/testPDR');
 const testPdrRoutes = require('./routes/testPDR-routes');
 
 const PORT = 3000;
-const URL = 'mongodb://0.0.0.0:27017/testPDRbox';
+const URL = 'mongodb+srv://texas:Mongodb333@cluster0.fjujydy.mongodb.net/testPDRbox?retryWrites=true&w=majority';
 
 const app = express();
 app.use(express.json());
 app.use(testPdrRoutes);
 
 mongoose
-    .connect(URL)
+    .connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log(`DB connection error: ${err}`))
 
