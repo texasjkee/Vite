bf53286aad8ce3c02ce6c16dd8e0cbbaf2ed5108
Michael <gt.gluck@gmail.com>
2023-09-15 10:57:00 +0300

    fix: baseQuery

--- front/src/api/apiSlice.js
+++ front/src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  basequery: fetchBaseQuery({ baseUrl: 'http://localhost:4000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000' }),
   endpoints: builder => ({
     getHeroes: builder.query({
       query: () => '/heroes'
